493d7c09c1a3944600f957e89289f1e827dfec2e
aparnajyothi-y <147696841+aparnajyothi-y@users.noreply.github.com>
2024-07-11 12:58:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 // Plugins
-import Vue from '@vitejs/plugin-vue'
-import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import Vuetify from 'vite-plugin-vuetify'
 
 // Utilities
 import { fileURLToPath, URL } from 'node:url'
@@ -9,17 +8,15 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [
-    Vue({
-      template: { transformAssetUrls },
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    // Removed Vue plugin
+    // Vue({
+    //   template: { transformAssetUrls },
+    // }),
     Vuetify(),
-    // Components(), // Removed this line
   ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      
     },
     rollupOptions: {
       external: ['vue'],
